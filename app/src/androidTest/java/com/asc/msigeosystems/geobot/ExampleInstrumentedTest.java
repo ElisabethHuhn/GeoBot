62b011f393ed4cfaaed4f8290510aef270c39954
ElisabethHuhn <Elisabethhuhn@gmail.com>
2017-12-11 14:14:31 -0500

    Switched Package name back and forth, GeoBot restored

--- app/src/androidTest/java/com/asc/msigeosystems/geobot/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/asc/msigeosystems/geobot/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.asc.msigeosystems.geobot", appContext.getPackageName());
+        assertEquals("com.asc.msigeosystems.coordconv", appContext.getPackageName());
     }
 }
